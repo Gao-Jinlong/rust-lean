dc553d4d4481e98867f53c1288d66ff32d7d57a9
gao-jinlong <2675130594@qq.com>
2023-09-12 00:16:21 +0800

    feat(lifetime)

--- my_rust/src/my_lifetime.rs
+++ my_rust/src/my_lifetime.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, string};
+
 pub fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
@@ -7,10 +9,34 @@ pub fn main() {
     //     let result = longest(string1.as_str(), string2.as_str());
     // }
     let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result)
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years age...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// 结构体定义中的生命周期注解
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+// 结合泛型类型参数，trait bounds 和生命周期
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
