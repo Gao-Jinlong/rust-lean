bfd22f809c17017f2ab20ad8aa1aa1ada755062e
gao-jinlong <2675130594@qq.com>
2023-09-18 23:21:20 +0800

    feat(mini_grep):TDD

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,16 +7,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parasing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}.", config.query);
-    println!("In file {}.", config.file_path);
-
     // 使用 if 来检查 run 是否返回一个 Err 值
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     };
 }
