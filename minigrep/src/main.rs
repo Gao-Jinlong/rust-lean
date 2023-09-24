2cd1676fe0f03214ddb41996948c494243189fd4
gao-jinlong <2675130594@qq.com>
2023-09-24 11:41:26 +0800

    feat(closure)

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,11 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}"); // 将错误输出到 stderr 而不是 stdout，使用 cargo run > output.txt 将正常输出写入文件，而错误信息仍然会显示在终端上
         process::exit(1);
     });
 
     // 使用 if 来检查 run 是否返回一个 Err 值
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
