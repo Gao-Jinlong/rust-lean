cb7ecab69247b14da6fba2383a61610d3ee4d80d
Ginlon <2675130594@qq.com>
2024-04-20 00:21:10 +0800

    feat: crate TDD

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,51 +1,134 @@
-//! # 注释包含项
-//! 这通常用于 crate 根文件（通常是 src/lib.rs）或模块的根文件为 crate 或模块整体提供文档。
+// //! # 注释包含项
+// //! 这通常用于 crate 根文件（通常是 src/lib.rs）或模块的根文件为 crate 或模块整体提供文档。
 
-use std::env;
+// use std::env;
+// use std::error::Error;
+// use std::fs;
+
+// pub struct Config {
+//     pub query: String,
+//     pub file_path: String,
+//     pub ignore_case: bool,
+// }
+
+// impl Config {
+//     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+//         args.next(); // skip the first argument, which is the program name
+
+//         let query = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a query string"),
+//         };
+//         let file_path = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a file path"),
+//         };
+
+//         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+//         Ok(Config {
+//             query,
+//             file_path,
+//             ignore_case,
+//         })
+//     }
+// }
+// /// # 文档注释
+// /// 可显示在 cargo doc --open 生成的文档中
+// /// 支持 markdown 语法
+// pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//     let contents = fs::read_to_string(config.file_path)?;
+
+//     let results = if config.ignore_case {
+//         search_case_insensitive(&config.query, &contents)
+//     } else {
+//         search(&config.query, &contents)
+//     };
+
+//     for line in results {
+//         println!("{line}");
+//     }
+
+//     Ok(())
+// }
+
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     contents
+//         .lines()
+//         .filter(|line| line.contains(query))
+//         .collect()
+// }
+
+// pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     let query = query.to_lowercase();
+//     let mut results = Vec::new();
+
+//     for line in contents.lines() {
+//         if line.to_lowercase().contains(&query) {
+//             results.push(line);
+//         }
+//     }
+
+//     results
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn case_sensitive() {
+//         let query = "duct";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Duct tape.";
+
+//         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+//     }
+
+//     #[test]
+//     fn case_insensitive() {
+//         let query = "rUsT";
+//         let contents = "\
+// Rust
+// safe, fast, productive.
+// Pick three.
+// Trust me.
+//         ";
+
+//         assert_eq!(
+//             vec!["Rust", "Trust me."],
+//             search_case_insensitive(query, contents)
+//         );
+//     }
+// }
+
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
-    pub ignore_case: bool,
 }
-
 impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next(); // skip the first argument, which is the program name
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file path"),
-        };
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
+        Ok(Config { query, file_path })
     }
 }
-/// # 文档注释
-/// 可显示在 cargo doc --open 生成的文档中
-/// 支持 markdown 语法
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-
-    for line in results {
+    for line in search(&config.query, &contents) {
         println!("{line}");
     }
 
@@ -53,22 +136,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
+    let mut results = vec![];
 
     for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
+        if line.contains(query) {
             results.push(line);
         }
     }
-
     results
 }
 
@@ -77,30 +151,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn case_sensitive() {
+    fn one_result() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.
-Duct tape.";
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust
-safe, fast, productive.
-Pick three.
-Trust me.
-        ";
-
-        assert_eq!(
-            vec!["Rust", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,5 @@
 use std::env;
 use std::process;
-use std::fs;
-use std::error::Error;
 
 // use lib::Config;
 
@@ -20,55 +18,30 @@ use std::error::Error;
 //     };
 // }
 
-fn main(){
+mod lib;
 
+use lib::Config;
+
+fn main() {
     let args: Vec<String> = env::args().collect();
 
     // unwrap_or_else 用于处理 Result 的 Ok 和 Err 两种情况
-    let config = Config::build(&args).unwrap_or_else(|err|{
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}",config.query);
-    println!("In file {}",config.file_path);
-
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     // run(config).unwrap_or_else(|err|{
     //     println!("Application error: {err}");
     //     process::exit(1);
     // })
-    
+
     // if let 控制流 用于处理只关心一个分支的情况
-    if let Err(e) = run(config) {
+    if let Err(e) = lib::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-
- 
-} 
-
-fn run(config:Config)-> Result<(),Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config{
-    query:String,
-    file_path:String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
