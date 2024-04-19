11f15226f29cde25475196ec90fba88e476ae04b
Ginlon <2675130594@qq.com>
2024-04-19 14:32:55 +0800

    feat:minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -21,16 +21,28 @@ mod lib;
 
 fn main(){
 
-    let args:Vec<String> = env::args().collect();
+    let args: Vac<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}",file_path);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 } 
+
+struct Config{
+    query:String,
+    file_path:String,
+}
+
+fn parse_config(args:&[String]) -> (&str, &str){
+    // TODO: 目前使用 clone() 方法来复制字符串，可以避免所有权问题，但并不是性能的最优解
+    let query = &args[1].clone();
+    let file_path =&args[2].clone();
+
+    Config{ query, file_path}
+}
