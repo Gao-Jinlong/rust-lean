de8667836c4e43ecd80eabc062e51ffbd9440065
Ginlon <2675130594@qq.com>
2024-04-21 22:37:03 +0800

    feat:closure

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,17 +1,131 @@
-use std::thread;
+// use std::thread;
 
-mod my_iterator;
+// mod my_iterator;
+
+// #[derive(Debug, PartialEq, Copy, Clone)]
+// enum ShirtColor {
+//     Red,
+//     Blue,
+// }
+
+// struct Inventory {
+//     shirts: Vec<ShirtColor>,
+// }
+
+// impl Inventory {
+//     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+//         user_preference.unwrap_or_else(|| self.most_stocked())
+//     }
+
+//     fn most_stocked(&self) -> ShirtColor {
+//         let mut num_red = 0;
+//         let mut num_blue = 0;
+
+//         for color in &self.shirts {
+//             match color {
+//                 ShirtColor::Red => num_red += 1,
+//                 ShirtColor::Blue => num_blue += 1,
+//             }
+//         }
+
+//         if num_red > num_blue {
+//             ShirtColor::Red
+//         } else {
+//             ShirtColor::Blue
+//         }
+//     }
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: i32,
+//     height: i32,
+// }
+
+// fn main() {
+//     let store = Inventory {
+//         shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Blue],
+//     };
+
+//     let user_pref1 = Some(ShirtColor::Red);
+//     let giveaway1 = store.giveaway(user_pref1);
+//     println!(
+//         "The user with preference {:?} gets {:?}",
+//         user_pref1, giveaway1
+//     );
+
+//     let user_pref2 = None;
+//     let giveaway2 = store.giveaway(user_pref2);
+//     println!(
+//         "The user with preference {:?} gets {:?}",
+//         user_pref2, giveaway2
+//     );
+
+//     let mut list = vec![1, 2, 3];
+//     println!("Before defining closure {:?}", list);
+
+//     let mut borrows_mutably = || list.push(7);
+
+//     borrows_mutably();
+//     println!("After calling closure {:?}", list);
+
+//     let list = [1, 2, 3];
+//     println!("Before defining closure: {:?}", list);
+
+//     thread::spawn(move || println!("From thread: {:?}", list))
+//         .join()
+//         .unwrap();
+
+//     let mut list = [
+//         Rectangle {
+//             width: 10,
+//             height: 1,
+//         },
+//         Rectangle {
+//             width: 3,
+//             height: 5,
+//         },
+//         Rectangle {
+//             width: 7,
+//             height: 12,
+//         },
+//     ];
+
+//     // sort_by_key 被定义为接收一个 FnMut 的闭包，这样就可以多次调用这个 FnMut 闭包，而不是只调用一次。
+//     list.sort_by_key(|r| r.width);
+//     println!("{:#?}", list);
+
+//     // FnOnce 闭包
+//     let mut sort_operations = vec![];
+//     let value = String::from("by key called");
+
+//     list.sort_by_key(|r| {
+//         // 如果使用下面的代码，会报错，因为 value 已经被 move 了
+//         // 这个闭包只能被调用一次
+//         // sort_operations.push(value);
+//         sort_operations.push(value.clone());
+//         r.width
+//     });
+//     println!("{:#?}", list);
+
+//     let mut num_sort_operations = 0;
+//     list.sort_by_key(|r| {
+//         num_sort_operations += 1;
+//         r.width
+//     });
+//     println!("{:#?}, sorted in {num_sort_operations} operations", list);
+
+//     my_iterator::main();
+// }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
     Blue,
 }
-
 struct Inventory {
     shirts: Vec<ShirtColor>,
 }
-
 impl Inventory {
     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
         user_preference.unwrap_or_else(|| self.most_stocked())
@@ -36,15 +150,9 @@ impl Inventory {
     }
 }
 
-#[derive(Debug)]
-struct Rectangle {
-    width: i32,
-    height: i32,
-}
-
 fn main() {
     let store = Inventory {
-        shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Blue],
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
     };
 
     let user_pref1 = Some(ShirtColor::Red);
@@ -60,60 +168,4 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_pref2, giveaway2
     );
-
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure {:?}", list);
-
-    let mut borrows_mutably = || list.push(7);
-
-    borrows_mutably();
-    println!("After calling closure {:?}", list);
-
-    let list = [1, 2, 3];
-    println!("Before defining closure: {:?}", list);
-
-    thread::spawn(move || println!("From thread: {:?}", list))
-        .join()
-        .unwrap();
-
-    let mut list = [
-        Rectangle {
-            width: 10,
-            height: 1,
-        },
-        Rectangle {
-            width: 3,
-            height: 5,
-        },
-        Rectangle {
-            width: 7,
-            height: 12,
-        },
-    ];
-
-    // sort_by_key 被定义为接收一个 FnMut 的闭包，这样就可以多次调用这个 FnMut 闭包，而不是只调用一次。
-    list.sort_by_key(|r| r.width);
-    println!("{:#?}", list);
-
-    // FnOnce 闭包
-    let mut sort_operations = vec![];
-    let value = String::from("by key called");
-
-    list.sort_by_key(|r| {
-        // 如果使用下面的代码，会报错，因为 value 已经被 move 了
-        // 这个闭包只能被调用一次
-        // sort_operations.push(value);
-        sort_operations.push(value.clone());
-        r.width
-    });
-    println!("{:#?}", list);
-
-    let mut num_sort_operations = 0;
-    list.sort_by_key(|r| {
-        num_sort_operations += 1;
-        r.width
-    });
-    println!("{:#?}, sorted in {num_sort_operations} operations", list);
-
-    my_iterator::main();
 }
