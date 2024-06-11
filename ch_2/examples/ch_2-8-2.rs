fb7594d2a4d724175bc46eb77b8e3747e1ec061c
Ginlon <2675130594@qq.com>
2024-06-11 12:52:07 +0800

    feat(Rust Course):ch_2-8-2

--- ch_2/examples/ch_2-8-2.rs
+++ ch_2/examples/ch_2-8-2.rs
@@ -1,3 +1,4 @@
+// 孤儿规则（orphan rule） / 相干性（coherence）
 // 如果想要为类型 A 实现特征 T，那么 A 或者 T 至少有一个是在当前作用域中定义的
 // 如无法为 String 类型实现 Display 特征，因为 String 和 Display 都是在标准库中定义的
 
@@ -11,7 +12,12 @@ use std::fmt::Display;
 // }
 
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    // 默认实现
+    fn summarize(&self) -> String {
+        format!("Read more from {}...", self.summarize_author())
+    }
 }
 pub struct Post {
     pub title: String,
@@ -19,8 +25,8 @@ pub struct Post {
     pub content: String,
 }
 impl Summary for Post {
-    fn summarize(&self) -> String {
-        format!("{} by {}", self.title, self.author)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 pub struct Weibo {
@@ -28,9 +34,33 @@ pub struct Weibo {
     pub content: String,
 }
 impl Summary for Weibo {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
+    fn summarize(&self) -> String {
+        format!("{} by {}", self.content, self.summarize_author())
+    }
+}
+
+// 使用特征作为函数参数
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+// impl Trait 只是一个语法糖
+// 完整写法
+pub fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+// 多个 Trait
+pub fn notify3(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+// where 从句
+pub fn notify4<T>(item: &T)
+where
+    T: Summary + Display,
+{
+    println!("Breaking news! {}", item.summarize());
 }
 
 fn main() {
@@ -45,4 +75,7 @@ fn main() {
     };
     println!("{}", post.summarize());
     println!("{}", weibo.summarize());
+
+    notify(&post);
+    notify(&weibo);
 }
